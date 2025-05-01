e1101beefcc313d7802312bf5b213f2f671ebf13
Maksim <maximus.kor53@gmail.com>
2025-05-01 22:41:20 +0700

    remove chakra

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,16 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Provider } from '@/components/ui/provider'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider>
-        <App />
-      </Provider>
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 )
